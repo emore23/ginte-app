5fa6dee2c64c1e619fdeff3c06688bdc43c6498c
Estefani Moré <estefani.muller.more@gmail.com>
2025-04-02 15:53:03 -0300

    feat: added readme and fixed files from build

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { AuthProvider } from '@/contexts/AuthContext';
 import { queryClient } from '@/services/api/query-client';
 
 interface ProvidersProps {
@@ -14,11 +13,9 @@ interface ProvidersProps {
 export const Providers = ({ children }: ProvidersProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Toaster position="top-right" />
-        {children}
-        <ReactQueryDevtools initialIsOpen={false} position="right" />
-      </AuthProvider>
+      <Toaster position="top-right" />
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} position="right" />
     </QueryClientProvider>
   );
 };
